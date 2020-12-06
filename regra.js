f3e0e5d9ce6c8e9af2b276266be3839e1fb62dfe
joaopaulojpos <jpo.oliveiras@gmail.com>
2020-12-06 20:23:08 -0300

    subindo calculadora

---
+++ regra.js
@@ -0,0 +1,64 @@
+function calculo(val1, val2) {
+  valor_compra = parseInt(val1);
+  valor_entrada = val2;
+
+  var taxas = [4.81, 5.92, 7.06, 8.21, 9.40, 11.11, 13.11, 14.40, 15.73, 17.08, 18.47, 19.89];
+  let taxa_debito = 3.08;
+  var valor_parcelas = [];
+  var valor_total = [];
+
+  var valor = valor_compra - valor_entrada;
+
+  //parte da taxa de débito
+  let percentual_debito = (taxa_debito * valor) / 100;
+  let debito_total = valor + percentual_debito;
+  
+  var i = 0;
+  while (taxas[i]) {
+    
+    let percentual = (taxas[i] * valor) / 100
+    valor_total.push(valor + percentual); //Populando os valores totais
+    valor_parcelas.push((valor + percentual) / (i+1)); //Populando os valores das parcelas
+    i++;
+  }
+
+  var table = "";
+  for(var i in valor_parcelas){
+    table += "<tr>" + "<td>" + (parseInt(i) + 1) + " x" + "</td>" + "<td>" + valor_parcelas[i].toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) + "</td>" + "<td>" + 
+    valor_total[i].toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) + "</td></tr>";
+  }
+
+  debito_linha = "<tr>" + "<td>" + "Débito " + "</td>" + "<td>" + debito_total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) + "</td>" + "<td>" + 
+  debito_total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) + "</td></tr>";
+
+  document.getElementById("exibir").innerHTML = debito_linha + table;
+
+}
+
+var text = document.getElementById("val_compra");
+var entrada = document.getElementById("val_entrada");
+
+text.addEventListener("keyup", function () {
+  var val_compra = this.value;
+  var val_entrada = document.getElementById("val_entrada").innerHTML
+  calculo(val_compra, val_entrada);
+})
+
+entrada.addEventListener("keyup", function () {
+  var val_compra = text.value
+  val_entrada = this.value;
+  calculo(val_compra, val_entrada);
+})
+
+function keyPressed(evt){
+  evt = evt || window.event;
+  var key = evt.keyCode || evt.which;
+  return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(evt) {
+  var str = keyPressed(evt);
+  
+  if(str == ',' | str == '.')
+      alert("Por favor apague e digite apenas números");
+};
